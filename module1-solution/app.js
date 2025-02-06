ecd66b39045a3384b2a68f5f75b234fb5818c7e2
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-02-06 23:02:21 +0530

    Fixed folder structure and moved files to root

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,3 +1,4 @@
+
 (function () {
     'use strict';
   
@@ -10,22 +11,18 @@
       $scope.message = '';
   
       $scope.checkLunchItems = function () {
-        // Trim the input and check if it's empty
         var items = $scope.lunchItems.trim();
         if (!items) {
           $scope.message = 'Please enter data first';
           return;
         }
   
-        // Split the input into an array of items
         var itemArray = items.split(',');
   
-        // Remove empty items (bonus feature)
         itemArray = itemArray.filter(function (item) {
           return item.trim() !== '';
         });
   
-        // Check the number of items and set the message
         if (itemArray.length <= 3) {
           $scope.message = 'Enjoy!';
         } else {
